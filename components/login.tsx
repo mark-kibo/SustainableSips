b4c63bf3d7ba4f979a8b03664c2a304d3519cb1f
mwairimu <mwairimu@fintech-group.com>
2024-01-03 09:25:29 +0300

    created login component and page

---
+++ components/login.tsx
@@ -0,0 +1,40 @@
+// components/Login.tsx
+import React, { useState } from 'react';
+
+const Login: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement your login logic here
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ pages/login.tsx
@@ -0,0 +1,15 @@
+// pages/login.tsx
+import React from 'react';
+import Login from '../components/login'
+
+
+const LoginPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <Login />
+        </div>
+    );
+};
+
+export default LoginPage;
