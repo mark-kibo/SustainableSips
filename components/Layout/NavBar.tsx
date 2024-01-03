05731626cc27aff0e69f04364aea8cf7a677b549
mark-kibo <kibochamark@gmail.com>
2024-01-03 09:56:36 +0300

    Feat:added search functionality

--- components/Layout/NavBar.tsx
+++ components/Layout/NavBar.tsx
@@ -5,13 +5,13 @@ import Image from 'next/image'
 import { FaBars } from 'react-icons/fa'
 import { SideNavContext } from '@/context/SideNavContext'
 import { Tooltip } from '@mui/material'
-import { LogOutIcon } from 'lucide-react'
+import { LogOutIcon, SearchIcon } from 'lucide-react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/shad/ui/dropdown-menu'
 
 const NavBar = () => {
     const { dispatch } = useContext(SideNavContext)
     return (
-        <div className='w-full shadow-sm flex-1'>
+        <div className='w-full shadow-sm flex-1 sm:text-sm'>
             <nav className="flex items-center justify-between flex-wrap bg-white dark:bg-black p-4">
                 <div className='flex md:flex-row items-center justify-between gap-2'>
                     <div className='flex hover:cursor-pointer hover:scale-105 hover:shadow-md hover:rounded-md py-2 px-1 transition ease-in-out duration-75 hover:bg-gray-200 '>
@@ -22,13 +22,19 @@ const NavBar = () => {
 
                         }} />
                     </div>
-                    <h2 className='flex  items-center justify-start px-4 uppercase leading-4 text-orange-6 text-sm md:text-md'>Sustainable sips
+                    <h2 className='flex  items-center justify-start px-4 uppercase leading-4 text-orange-6 text-sm lg:text-md'>Sustainable sips
 
                     </h2>
 
 
                 </div>
-                <div className='flex justify-between mx-4'>
+                <div className='flex justify-between mx-4 pr-4'>
+                    <div className='p-2 flex-1 flex justify-between items-center shadow-md rounded-md gap-1'>
+                        
+                            <input name="search" id="" placeholder="Search..." className='w-full px-2 bg-white dark:text-black text-sm outline-none rounded-sm'/>
+                            <SearchIcon size={20} className='hover:cursor-pointer'/>
+                        
+                    </div>
                     <Theme />
                     {/* <Tooltip title="profile">
                         <Image
@@ -41,7 +47,9 @@ const NavBar = () => {
                             className='rounded-md shadow-md cursor-pointer'
                         />
                     </Tooltip> */}
-                    
+
+
+
                     <DropdownMenu >
                         <DropdownMenuTrigger>
                             <Tooltip title="profile">
@@ -60,10 +68,10 @@ const NavBar = () => {
                             <DropdownMenuLabel className='items-center dark:text-black'>My Account</DropdownMenuLabel>
                             <DropdownMenuSeparator />
                             <DropdownMenuItem className='hover:cursor-pointer px-2 hover:border-none hover:bg-gray-200 dark:text-black dark:hover:bg-gray-300'>Profile</DropdownMenuItem>
-                            <DropdownMenuItem className='hover:cursor-pointer px-2 hover:border-none hover:bg-gray-200 dark:text-black dark:hover:bg-gray-300'>Logout</DropdownMenuItem>        
+                            <DropdownMenuItem className='hover:cursor-pointer px-2 hover:border-none hover:bg-gray-200 dark:text-black dark:hover:bg-gray-300'>Logout</DropdownMenuItem>
                         </DropdownMenuContent>
                     </DropdownMenu>
-                    
+
 
 
                 </div>
