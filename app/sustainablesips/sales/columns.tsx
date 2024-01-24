84aa270afdeaec1c91b703150b29317bd87e6aa3
Mercy K. Wairimu <mwairimu@fintech-group.com>
2024-01-24 10:56:23 +0300

    fixed date display

--- app/sustainablesips/sales/columns.tsx
+++ app/sustainablesips/sales/columns.tsx
@@ -21,7 +21,6 @@ import { EditModalContext } from "@/context/ModalContext";
 export type Sale = {
     id: string;
     name: string;
-    // image: string;
     price: number;
     description: string;
 
@@ -32,23 +31,9 @@ export const columns: GridColDef[] = [
 
     { field: 'id', headerName: 'Identifier', width: 80, type: 'string',
 
-    // renderCell: (params) => {
-    //     // Transform UUID to a number using Number()
-    //     let [rowNumber, setRowNumber] = useState(0);
-    //     return rowNumber += 1; //
-    //   },
+    
 },
-    // {
-    //     field: 'image',
-    //     headerName: 'Image',
-    //     type: 'string',
-    //     width: 20,
-    //     renderCell: (params) => {
-    //         console.log(params)
-    //         const imageSrc = params.row.image as string;
-    //         return <Image src={imageSrc} width={30} height={30} alt="product image" className="rounded-full" />;
-    //     }
-    // },
+  
     {
         field: 'product_name', headerName: 'Product Name', width: 150, type: 'string'
     },
@@ -63,10 +48,10 @@ export const columns: GridColDef[] = [
     // { field: 'product', headerName: 'product', type: 'string', width: 150 },
     {
         field: 'date_created', headerName: 'Sale date', type: 'string', width: 150,
-
         renderCell: (params) => {
-            return new Date(params.row.date_created).toLocaleDateString()
+            const saleDate = new Date(params.row.created_at); // changed from date_created to created_at
+            return isNaN(saleDate.getTime()) ? "Invalid Date" : saleDate.toLocaleDateString();
         }
-    },
+    }
 
 ]
