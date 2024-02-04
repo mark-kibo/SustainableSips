f15285ee466e87b076b35cd6bdc3f0d1c8fd776f
Mercy K. Wairimu <mwairimu@fintech-group.com>
2024-02-04 09:53:25 +0300

    restricted the action buttons

--- components/users/UserDatatable.tsx
+++ components/users/UserDatatable.tsx
@@ -4,10 +4,9 @@ import Box from '@mui/material/Box';
 import {
     DataGrid,
     GridToolbar,
-    GridRowsProp,
-    GridColDef,
     GridFilterModel,
     GridColumnVisibilityModel,
+    GridColDef,
 } from '@mui/x-data-grid';
 
 import React, { useContext } from 'react';
@@ -21,77 +20,76 @@ import {
 import { EditModalContext } from '@/context/ModalContext';
 import { FaDownload, FaEdit, FaTrash } from 'react-icons/fa';
 import { DeleteRequest } from './DeleteRequest';
+import { useSession } from 'next-auth/react';
+import { jwtDecode } from 'jwt-decode';
 
 export default function UserDataTable({ data, columns }: { data: any, columns: GridColDef[] }) {
     const [filterModel, setFilterModel] = React.useState<GridFilterModel>({
         items: [],
         quickFilterExcludeHiddenColumns: true,
     });
+    const { data: session } = useSession();
+    console.log(session?.user);
 
-    const { onOpen, setId } = useContext(EditModalContext)
+    let role = "";
+    if (session) {
+        const decodedToken = jwtDecode(session?.user?.userToken);
+        role = decodedToken.role;
+    }
 
+    const { onOpen, setId } = useContext(EditModalContext);
 
-    const [columnVisibilityModel, setColumnVisibilityModel] =
-        React.useState<GridColumnVisibilityModel>({});
-
-    const actions = [{
-        field: 'actions', headerName: 'Actions', type: 'string', width: 150,
-        renderCell: (params: { row: any; }) => {
+    const actions: GridColDef[] = [{
+        field: 'actions',
+        headerName: 'Actions',
+        type: 'string',
+        width: 200,
+        renderCell: (params: { row: any }) => {
             return (
                 <div className='flex items-start ml-2 justify-between gap-4 text-black'>
-                    
-                    <p className='shadow-sm rounded-sm text-primary-800 bg-white p-2 hover:bg-orange-300 cursor-pointer'
-                        onClick={()=>{
-                            onOpen()
-                            setId(params.row)
-                        }}
-                    ><FaEdit /></p>
-                    <p className='shadow-sm rounded-sm text-error-600 bg-white p-2 hover:bg-orange-300 cursor-pointer' onClick={() => {
-                        DeleteRequest(params.row.id)
-                    }}><FaTrash /></p>
-                    
+                    {Number(role) === 1 && (
+    <>
+        <p className='shadow-sm rounded-sm text-primary-800 bg-white p-2 hover:bg-orange-300 cursor-pointer'
+            onClick={() => {
+                onOpen();
+                setId(params.row);
+            }}
+        ><FaEdit /></p>
+        <p className='shadow-sm rounded-sm text-error-600 bg-white p-2 hover:bg-orange-300 cursor-pointer' onClick={() => {
+            DeleteRequest(params.row.id);
+        }}><FaTrash /></p>
+    </>
+)}
+
                 </div>
-            )
-
-
-
+            );
         },
-    }]
+    }];
 
-    console.log(data)
 
     return (
-        <div className='w-full ' >
-            <Box sx={{ height: 400, }}>
+        <div className='w-full'>
+            <Box sx={{ height: 400 }}>
                 <DataGrid
-                   columns={[...columns.map(col => ({
-                    ...col,
-                    headerClassName: 'font-bold text-black-1000 capitalize'
-                  })), ...actions]}
+                    columns={[...columns.map(col => ({
+                        ...col,
+                        headerClassName: 'font-bold text-black-1000 capitalize'
+                    })), ...actions]}
                     rows={data}
                     disableColumnFilter
-                    // disableColumnMenu
                     disableDensitySelector
                     slots={{ toolbar: GridToolbar }}
                     filterModel={filterModel}
                     onFilterModelChange={(newModel) => setFilterModel(newModel)}
                     slotProps={{ toolbar: { showQuickFilter: true } }}
-                    // columnVisibilityModel={columnVisibilityModel}
-                    onColumnVisibilityModelChange={(newModel) =>
-                        setColumnVisibilityModel(newModel)
-                    }
                     initialState={{
-
                         pagination: { paginationModel: { pageSize: 5 } },
                     }}
-
                     pageSizeOptions={[5, 10, 25]}
                     sx={{
                         border: "none",
-
-
                     }}
-                    getRowId={(row)=> row.id}
+                    getRowId={(row) => row.id}
                     className='shadow-lg rounded-md  dark:text-white dark:border dark:border-white'
                 />
             </Box>
